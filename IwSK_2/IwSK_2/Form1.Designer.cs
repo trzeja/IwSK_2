3ca166084b57da28779e9b052192dfcf7043890a
iko <trzeja.mikolaj@gmail.com>
2017-09-24 13:56:04 +0200

    Dodane parametry dla Master

--- IwSK_2/IwSK_2/Form1.Designer.cs
+++ IwSK_2/IwSK_2/Form1.Designer.cs
@@ -44,10 +44,22 @@
             this.cbCommands = new System.Windows.Forms.ComboBox();
             this.tbArguments = new System.Windows.Forms.TextBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.nudTimeout = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nudRetransmissions = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.nudTimeConstraint = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
             this.gbMaster.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRetransmissions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeConstraint)).BeginInit();
             this.SuspendLayout();
             // 
             // rbMaster
@@ -90,7 +102,7 @@
             this.gbMaster.Controls.Add(this.groupBox3);
             this.gbMaster.Location = new System.Drawing.Point(12, 12);
             this.gbMaster.Name = "gbMaster";
-            this.gbMaster.Size = new System.Drawing.Size(319, 267);
+            this.gbMaster.Size = new System.Drawing.Size(319, 419);
             this.gbMaster.TabIndex = 3;
             this.gbMaster.TabStop = false;
             this.gbMaster.Text = "Master";
@@ -99,7 +111,7 @@
             // 
             this.groupBox2.Location = new System.Drawing.Point(444, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(319, 267);
+            this.groupBox2.Size = new System.Drawing.Size(319, 419);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Slave";
@@ -110,7 +122,7 @@
             this.groupBox3.Controls.Add(this.rbAddressed);
             this.groupBox3.Location = new System.Drawing.Point(6, 19);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(103, 79);
+            this.groupBox3.Size = new System.Drawing.Size(103, 118);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Transaction mode";
@@ -118,7 +130,7 @@
             // rbBroadcast
             // 
             this.rbBroadcast.AutoSize = true;
-            this.rbBroadcast.Location = new System.Drawing.Point(6, 55);
+            this.rbBroadcast.Location = new System.Drawing.Point(6, 70);
             this.rbBroadcast.Name = "rbBroadcast";
             this.rbBroadcast.Size = new System.Drawing.Size(73, 17);
             this.rbBroadcast.TabIndex = 1;
@@ -129,7 +141,7 @@
             // rbAddressed
             // 
             this.rbAddressed.AutoSize = true;
-            this.rbAddressed.Location = new System.Drawing.Point(6, 32);
+            this.rbAddressed.Location = new System.Drawing.Point(6, 38);
             this.rbAddressed.Name = "rbAddressed";
             this.rbAddressed.Size = new System.Drawing.Size(75, 17);
             this.rbAddressed.TabIndex = 0;
@@ -204,18 +216,117 @@
             // 
             // groupBox5
             // 
-            this.groupBox5.Location = new System.Drawing.Point(6, 104);
+            this.groupBox5.Controls.Add(this.label7);
+            this.groupBox5.Controls.Add(this.label8);
+            this.groupBox5.Controls.Add(this.nudTimeConstraint);
+            this.groupBox5.Controls.Add(this.label6);
+            this.groupBox5.Controls.Add(this.nudRetransmissions);
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.nudTimeout);
+            this.groupBox5.Location = new System.Drawing.Point(6, 148);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(103, 157);
+            this.groupBox5.Size = new System.Drawing.Size(307, 157);
             this.groupBox5.TabIndex = 4;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Parameters";
+            // 
+            // nudTimeout
+            // 
+            this.nudTimeout.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nudTimeout.Location = new System.Drawing.Point(98, 29);
+            this.nudTimeout.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudTimeout.Name = "nudTimeout";
+            this.nudTimeout.Size = new System.Drawing.Size(120, 20);
+            this.nudTimeout.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Timeout:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "ms";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Retransmissions:";
+            // 
+            // nudRetransmissions
+            // 
+            this.nudRetransmissions.Location = new System.Drawing.Point(98, 56);
+            this.nudRetransmissions.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudRetransmissions.Name = "nudRetransmissions";
+            this.nudRetransmissions.Size = new System.Drawing.Size(120, 20);
+            this.nudRetransmissions.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(224, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "ms";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Time constraint:";
+            // 
+            // nudTimeConstraint
+            // 
+            this.nudTimeConstraint.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudTimeConstraint.Location = new System.Drawing.Point(98, 84);
+            this.nudTimeConstraint.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudTimeConstraint.Name = "nudTimeConstraint";
+            this.nudTimeConstraint.Size = new System.Drawing.Size(120, 20);
+            this.nudTimeConstraint.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(773, 306);
+            this.ClientSize = new System.Drawing.Size(773, 443);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.gbMaster);
             this.Controls.Add(this.groupBox1);
@@ -228,6 +339,11 @@
             this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRetransmissions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeConstraint)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -250,6 +366,14 @@
         private System.Windows.Forms.TextBox tbArguments;
         private System.Windows.Forms.ComboBox cbCommands;
         private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.NumericUpDown nudTimeout;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown nudRetransmissions;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown nudTimeConstraint;
     }
 }
 
