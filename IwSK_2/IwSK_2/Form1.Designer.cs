05e0615139a63f1f814965594652de4e6dcbf84e
iko <trzeja.mikolaj@gmail.com>
2017-09-24 09:52:29 +0200

    Kolejne kontrolki

--- IwSK_2/IwSK_2/Form1.Designer.cs
+++ IwSK_2/IwSK_2/Form1.Designer.cs
@@ -28,38 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rbMaster = new System.Windows.Forms.RadioButton();
+            this.rbSlave = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbMaster = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rbBroadcast = new System.Windows.Forms.RadioButton();
+            this.rbAddressed = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
+            this.gbMaster.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // radioButton1
+            // rbMaster
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 19);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(57, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Master";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rbMaster.AutoSize = true;
+            this.rbMaster.Location = new System.Drawing.Point(6, 19);
+            this.rbMaster.Name = "rbMaster";
+            this.rbMaster.Size = new System.Drawing.Size(57, 17);
+            this.rbMaster.TabIndex = 0;
+            this.rbMaster.TabStop = true;
+            this.rbMaster.Text = "Master";
+            this.rbMaster.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // rbSlave
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(6, 42);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(52, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Slave";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rbSlave.AutoSize = true;
+            this.rbSlave.Location = new System.Drawing.Point(6, 42);
+            this.rbSlave.Name = "rbSlave";
+            this.rbSlave.Size = new System.Drawing.Size(52, 17);
+            this.rbSlave.TabIndex = 1;
+            this.rbSlave.TabStop = true;
+            this.rbSlave.Text = "Slave";
+            this.rbSlave.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.rbSlave);
+            this.groupBox1.Controls.Add(this.rbMaster);
             this.groupBox1.Location = new System.Drawing.Point(337, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(101, 74);
@@ -67,25 +74,87 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Mode";
             // 
+            // gbMaster
+            // 
+            this.gbMaster.Controls.Add(this.groupBox3);
+            this.gbMaster.Location = new System.Drawing.Point(12, 12);
+            this.gbMaster.Name = "gbMaster";
+            this.gbMaster.Size = new System.Drawing.Size(319, 267);
+            this.gbMaster.TabIndex = 3;
+            this.gbMaster.TabStop = false;
+            this.gbMaster.Text = "Master";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(444, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(319, 267);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Slave";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rbBroadcast);
+            this.groupBox3.Controls.Add(this.rbAddressed);
+            this.groupBox3.Location = new System.Drawing.Point(6, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(103, 98);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Transaction mode";
+            // 
+            // rbBroadcast
+            // 
+            this.rbBroadcast.AutoSize = true;
+            this.rbBroadcast.Location = new System.Drawing.Point(6, 55);
+            this.rbBroadcast.Name = "rbBroadcast";
+            this.rbBroadcast.Size = new System.Drawing.Size(73, 17);
+            this.rbBroadcast.TabIndex = 1;
+            this.rbBroadcast.TabStop = true;
+            this.rbBroadcast.Text = "Broadcast";
+            this.rbBroadcast.UseVisualStyleBackColor = true;
+            // 
+            // rbAddressed
+            // 
+            this.rbAddressed.AutoSize = true;
+            this.rbAddressed.Location = new System.Drawing.Point(6, 32);
+            this.rbAddressed.Name = "rbAddressed";
+            this.rbAddressed.Size = new System.Drawing.Size(75, 17);
+            this.rbAddressed.TabIndex = 0;
+            this.rbAddressed.TabStop = true;
+            this.rbAddressed.Text = "Addressed";
+            this.rbAddressed.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 306);
+            this.ClientSize = new System.Drawing.Size(773, 306);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gbMaster);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "IwSK2";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.gbMaster.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rbMaster;
+        private System.Windows.Forms.RadioButton rbSlave;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbMaster;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rbBroadcast;
+        private System.Windows.Forms.RadioButton rbAddressed;
     }
 }
 
