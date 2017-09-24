cd7de25fcd88ce9ce143de390ac83d7859c591a9
iko <trzeja.mikolaj@gmail.com>
2017-09-24 12:50:11 +0200

    Kolejne kontrolki

--- IwSK_2/IwSK_2/Form1.Designer.cs
+++ IwSK_2/IwSK_2/Form1.Designer.cs
@@ -36,9 +36,18 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.rbBroadcast = new System.Windows.Forms.RadioButton();
             this.rbAddressed = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbAddress = new System.Windows.Forms.ComboBox();
+            this.cbCommands = new System.Windows.Forms.ComboBox();
+            this.tbArguments = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.gbMaster.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // rbMaster
@@ -76,6 +85,8 @@
             // 
             // gbMaster
             // 
+            this.gbMaster.Controls.Add(this.groupBox5);
+            this.gbMaster.Controls.Add(this.groupBox4);
             this.gbMaster.Controls.Add(this.groupBox3);
             this.gbMaster.Location = new System.Drawing.Point(12, 12);
             this.gbMaster.Name = "gbMaster";
@@ -99,7 +110,7 @@
             this.groupBox3.Controls.Add(this.rbAddressed);
             this.groupBox3.Location = new System.Drawing.Point(6, 19);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(103, 98);
+            this.groupBox3.Size = new System.Drawing.Size(103, 79);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Transaction mode";
@@ -126,6 +137,80 @@
             this.rbAddressed.Text = "Addressed";
             this.rbAddressed.UseVisualStyleBackColor = true;
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tbArguments);
+            this.groupBox4.Controls.Add(this.cbCommands);
+            this.groupBox4.Controls.Add(this.cbAddress);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Location = new System.Drawing.Point(115, 19);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(198, 118);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Frame definition";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Command:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Arguments:";
+            // 
+            // cbAddress
+            // 
+            this.cbAddress.FormattingEnabled = true;
+            this.cbAddress.Location = new System.Drawing.Point(69, 15);
+            this.cbAddress.Name = "cbAddress";
+            this.cbAddress.Size = new System.Drawing.Size(102, 21);
+            this.cbAddress.TabIndex = 3;
+            // 
+            // cbCommands
+            // 
+            this.cbCommands.FormattingEnabled = true;
+            this.cbCommands.Location = new System.Drawing.Point(69, 47);
+            this.cbCommands.Name = "cbCommands";
+            this.cbCommands.Size = new System.Drawing.Size(102, 21);
+            this.cbCommands.TabIndex = 4;
+            // 
+            // tbArguments
+            // 
+            this.tbArguments.Location = new System.Drawing.Point(69, 82);
+            this.tbArguments.Name = "tbArguments";
+            this.tbArguments.Size = new System.Drawing.Size(100, 20);
+            this.tbArguments.TabIndex = 5;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Location = new System.Drawing.Point(6, 104);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(103, 157);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Parameters";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -141,6 +226,8 @@
             this.gbMaster.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -155,6 +242,14 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.RadioButton rbBroadcast;
         private System.Windows.Forms.RadioButton rbAddressed;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbAddress;
+        private System.Windows.Forms.TextBox tbArguments;
+        private System.Windows.Forms.ComboBox cbCommands;
+        private System.Windows.Forms.GroupBox groupBox5;
     }
 }
 
