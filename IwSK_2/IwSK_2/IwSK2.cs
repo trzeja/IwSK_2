48b98d8e6cf1f4ba01e23cf6ca1e8d249162818b
Roksana Paluch <rokspal895@student.polsl.pl>
2017-09-26 02:14:39 +0200

    Tworzenie ramki, ale jeszcze nie do transmisji (konwersja na ascii jeszcze)

--- IwSK_2/IwSK_2/IwSK2.cs
+++ IwSK_2/IwSK_2/IwSK2.cs
@@ -47,7 +47,38 @@ namespace IwSK_2
 
         private void btnSendMaster_Click(object sender, EventArgs e)
         {
-
+            List<char> dataChar = new List<char>();
+            dataChar.Add(';');
+            int sum = 0;
+            int dec = Int32.Parse(tbAddress.Text);
+            
+            string address = dec.ToString("X");
+            for (int i = 0;i<address.Length;i++)
+            {
+                dataChar.Add(address.ElementAt(i));
+            }
+            for (int i = 0; i < tbTransmittedDataMasterHex.Text.Length; i++)
+            {
+                if (tbTransmittedDataMasterHex.Text.ElementAt(i) != ' ') {
+                    dataChar.Add(tbTransmittedDataMasterHex.Text.ElementAt(i));
+                }
+            }
+            sum += dec;
+            for (int i = 0;i<tbTransmittedDataMaster.Text.Length;i++)
+            {
+                string hex = ConvertStringToHex(tbTransmittedDataMaster.Text.ElementAt(i).ToString());
+                sum += Convert.ToInt32(hex,16);
+            }
+            byte[] sumByte = BitConverter.GetBytes(sum);
+            int sB = Convert.ToInt32(sumByte[0]);
+            sB = (255-sB)+1;
+            string lrc = sB.ToString("X");
+            for (int i = 0; i < lrc.Length; i++)
+            {
+                dataChar.Add(lrc.ElementAt(i));
+            }
+            dataChar.Add('\r');
+            dataChar.Add('\n');
         }
 
         private void btnConfigureSlave_Click(object sender, EventArgs e)
@@ -57,6 +88,7 @@ namespace IwSK_2
 
         private void btnSendSlave_Click(object sender, EventArgs e)
         {
+            
 
         }
 
